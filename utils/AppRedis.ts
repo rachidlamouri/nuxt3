ccb19f2187b345c8356dbc9f95f2662c0321c431
Abbas Lamouri <alamouri@GENVAC.local>
2023-04-05 12:03:01 -0400

    M

--- utils/AppRedis.ts
+++ utils/AppRedis.ts
@@ -1,23 +1,23 @@
-import { createClient } from 'redis'
+// import { createClient } from 'redis'
 
-class RedisClient {
-  // custom: boolean
-  client: any
-  constructor(url: string) {
-    this.client = createClient({ url })
-  }
+// class RedisClient {
+//   // custom: boolean
+//   client: any
+//   constructor(url: string) {
+//     this.client = createClient({ url })
+//   }
 
-  async connect() {
-    this.client.on('error', (err: Error) => console.log('Redis Client Error', err))
-    this.client.on('connect', () => console.log('redis connection succesfull'))
-    this.client.on('reday', () => console.log('redis client is ready'))
-    this.client.on('end', () => console.log('redis connection has closed'))
-    await this.client.connect()
-  }
+//   async connect() {
+//     this.client.on('error', (err: Error) => console.log('Redis Client Error', err))
+//     this.client.on('connect', () => console.log('redis connection succesfull'))
+//     this.client.on('reday', () => console.log('redis client is ready'))
+//     this.client.on('end', () => console.log('redis connection has closed'))
+//     await this.client.connect()
+//   }
 
-  async disconnect() {
-    await this.client.disconnect()
-  }
-}
+//   async disconnect() {
+//     await this.client.disconnect()
+//   }
+// }
 
-export default new RedisClient(process.env.NUXT_REDIS_URL as string)
+// export default new RedisClient(process.env.NUXT_REDIS_URL as string)
